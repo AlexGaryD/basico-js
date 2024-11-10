b16f3075f062825676766781bc9bf8d4bf9f9739
Alexis Vera S. <alxvcs93@hotmail.com>
2024-11-09 20:34:00 -0500

    hice cambios en la carpeta for-of y en el index.html y js

--- ciclos/loops/for-of/index.js
+++ ciclos/loops/for-of/index.js
@@ -1,15 +1,24 @@
 /*
+Bucle for-of en JavaScript
 
-for of arrays, strings [algo]
+El bucle for-of permite recorrer objetos iterables (como arrays o strings) de manera sencilla.
 
-for (varible of objeto) {
-  código 
+Sintaxis:
+for (variable of iterable) {
+    // Código a ejecutar en cada iteración
 }
-
 */
 
+// Ejemplo de bucle for-of que recorre un array de frutas
 let canasta = ["manzana", "pera", "naranja", "uva"];
 
-for (fruta of canasta) {
-  console.log(fruta);
+// Recorre cada elemento del array 'canasta'
+for (let fruta of canasta) {
+    console.log(fruta); // Imprime cada fruta en la consola
 }
+
+// Explicación detallada:
+// 1. 'let fruta of canasta':
+//    - Declara una variable `fruta` que tomará el valor de cada elemento del array `canasta` en cada iteración.
+// 2. 'console.log(fruta)':
+//    - En cada iteración, se imprime el valor actual de `fruta` en la consola.

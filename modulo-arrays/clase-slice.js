fa422c63d69979c7b6a6acb809ad05cf26b9436f
Alexis Vera S. <alxvcs93@hotmail.com>
2025-02-10 21:16:16 -0500

    Coloque los comentario a la clase Crear copias con slice( )

--- modulo-arrays/clase-slice.js
+++ modulo-arrays/clase-slice.js
@@ -1,10 +1,23 @@
-// slice()
+// slice() El método slice() devuelve una copia superficial de una porción de un array en un nuevo array.
+//No modifica el array original.
+
 
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
 
-console.log(animals.slice(2))
-console.log(animals.slice(2, 4))
-console.log(animals.slice(1, 6))
-console.log(animals.slice(-2))
-console.log(animals.slice(2, -1))
-console.log(animals.slice())
+console.log(animals.slice(2)) // Crea una copia del array desde el índice 2 hasta el final
+// Imprime: ['camel', 'duck', 'elephant']
+
+console.log(animals.slice(2, 4)) // Crea una copia del array desde el índice 2 hasta el índice 4 (sin incluirlo)
+// Imprime: ['camel', 'duck']
+
+console.log(animals.slice(1, 6)) // Crea una copia del array desde el índice 1 hasta el índice 6 (sin incluirlo)
+// Imprime: ['bison', 'camel', 'duck', 'elephant']
+
+console.log(animals.slice(-2)) // Crea una copia del array desde el segundo índice desde el final hasta el final
+// Imprime: ['duck', 'elephant']
+
+console.log(animals.slice(2, -1)) // Crea una copia del array desde el índice 2 hasta el penúltimo índice
+// Imprime: ['camel', 'duck']
+
+console.log(animals.slice()) // Crea una copia completa del array
+// Imprime: ['ant', 'bison', 'camel', 'duck', 'elephant']

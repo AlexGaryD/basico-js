67e500400b375fd9e076ff2aba75f9eb74a7e190
Alexis Vera S. <alxvcs93@hotmail.com>
2025-02-24 19:58:51 -0500

    Coloque los comentario a la clase Trabajando con objetos en JavaScript

--- obj&Classes/objetos/index.js
+++ obj&Classes/objetos/index.js
@@ -8,63 +8,45 @@ objeto {
   Metodos()
 }
 */
-
 const persona = {
-  nombre: "Gary", // Propiedad nombre con valor "John"
-  edad: 31, // Propiedad edad con valor 30
-  direccion: {
-    calle: "Av Condor Ñan", // Propiedad calle dentro del objeto direccion
-    ciudad: "Quito", // Propiedad ciudad dentro del objeto direccion
+  nombre: 'Gary',
+  edad: 31,
+  dirección: {
+    calle: "Av Condor Ñan 2165",
+    ciudad: "Quito"
   },
-  saludar() {
-    console.log(`hola, mi nombre es ${this.nombre}`); // Método saludar que imprime un mensaje con el nombre
+  saludar(){
+    console.log(`Hola mi nombre es $ {persona.nombre}`)
   },
-  mi_edad() {
-    console.log(`mi edad es ${this.edad}`); // Método miedad que imprime un mensaje con la edad
+  saludar(){
+    console.log(`Mi dirección es ${persona.dirección.calle}`);
   },
-  mi_direccion() {
-    console.log(`mi direccion es ${this.direccion.calle} en la ciudad de ${this.direccion.ciudad}`); // Método midireccion que imprime un mensaje con la calle y ciudad
-  },
+  saludar(){
+    console.log(`Mi edad es ${persona.edad}`);
+  }
+
 };
 
 // Imprimamos el objeto
+console.log(persona);
+persona.saludar();
 
-console.log(persona); // Imprime el objeto persona
+console.log(persona.dirección.calle);
+persona.saludar();
 
-// Imprimamos la propiedad nombre
+console.log(persona.edad);
+persona.saludar();
 
-console.log(persona.nombre); // Imprime la propiedad nombre del objeto persona
-
-// Imprimamos el metodo saludar
-
-persona.saludar(); // Llama al método saludar del objeto persona
-
-// Agrergamos una propiedad al objeto en este caso telefono
-
-persona.telefono = "555-555-5555"; // Agrega la propiedad telefono al objeto persona
-
-console.log(persona.telefono); // Imprime la propiedad telefono del objeto persona
-
-// Agregamos un metodo al objeto
+persona.telefono = "0987654321";
+console.log(persona.telefono);
 
 persona.despedir = () => {
-  console.log("Adios"); // Método despedir que imprime "Adios"
-};
+  console.log(`Adios ${persona.nombre}`);
+}
 
-persona.despedir(); // Llama al método despedir del objeto persona
+persona.despedir();
 
-// Acceder a una propiedad de un objeto anidado / Metodo
+delete persona.telefono;
 
-console.log(persona.direccion.calle); // Imprime la propiedad calle del objeto anidado direccion
-
-// Eliminar una propiedad de un objeto
-
-delete persona.telefono; // Elimina la propiedad telefono del objeto persona
-
-console.log(persona.telefono); // Intenta imprimir la propiedad telefono del objeto persona (debería ser undefined)
-
-// Eliminar un metodo de un objeto
-
-delete persona.despedir; // Elimina el método despedir del objeto persona
-
-console.log(persona.despedir); // Intenta imprimir el método despedir del objeto persona (debería ser undefined)
+delete persona.despedir;
+delete persona.saludar;

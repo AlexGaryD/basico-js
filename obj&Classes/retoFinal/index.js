0adc926ecf7beaecb783e1445269fc02c88a7875
Alexis Vera S. <alxvcs93@hotmail.com>
2025-03-10 19:49:32 -0500

    Realice cambios al proyecto final y coloque los comentarios como refetencia

--- obj&Classes/retoFinal/index.js
+++ obj&Classes/retoFinal/index.js
@@ -50,22 +50,33 @@ const usersTimeline = [
   },
 ];
 
+// Función para verificar si el usuario y contraseña existen en la base de datos
 function usuarioExistente(username, password) {
+  // Utiliza el método some() para verificar si algún usuario en la base de datos coincide con el nombre de usuario y la contraseña proporcionados
   return usersDatabase.some(user => user.username === username && user.password === password);
 }
 
+// Función para obtener el timeline del usuario
 function obtenerTimeline(username) {
+  // Utiliza el método find() para buscar el timeline del usuario en el array usersTimeline
   const userTimeline = usersTimeline.find(user => user.username === username);
+  // Retorna el timeline del usuario si se encuentra, de lo contrario retorna null
   return userTimeline ? userTimeline : null;
 }
 
+// Función para iniciar sesión
 function signIn() {
+  // Obtiene los valores de usuario y contraseña desde los campos de entrada del formulario
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
+  // Verifica si el usuario y contraseña existen en la base de datos
   if (usuarioExistente(username, password)) {
+    // Muestra un mensaje de bienvenida si el usuario y contraseña son correctos
     alert(`Bienvenido a tu cuenta ${username}`);
+    // Obtiene el timeline del usuario
     const userTimeline = obtenerTimeline(username);
+    // Si se encuentra el timeline del usuario, lo muestra en el contenedor del timeline
     if (userTimeline) {
       document.getElementById("timeline").innerHTML = `
         <img src="${userTimeline.image}" alt="${username}">
@@ -73,7 +84,9 @@ function signIn() {
       `;
     }
   } else {
+    // Muestra un mensaje de error si el usuario o contraseña son incorrectos
     alert("Uuups, usuario o contraseña incorrectos!");
+    // Limpia el contenido del contenedor del timeline
     document.getElementById("timeline").innerHTML = "";
   }
 }

432e67730125c024dc4c6e13fadbe2a68b3637a1
Alexis Vera S. <alxvcs93@hotmail.com>
2025-03-11 20:24:39 -0500

    Cambios en el archivo promesas.js

--- asincronia-js/Promesas/promesas.js
+++ asincronia-js/Promesas/promesas.js
@@ -1,17 +1,22 @@
 /*
 Promise ()
-Estados de la promesa:
-- pending: cuando la promesa se va a crear
-- fulfilled: cuando se resuelve la promesa
-- rejected: cuando la promesa no se resolvió
+Las promesas presentan tres estados fundamentales durante su ciclo de vida:
 
-Callbacks que tienen las promesas:
-- resolve: se llama cuando la promesa se resuelve
-- reject: se llama cuando la promesa no se resuelve
+Pending (pendiente): Este es el estado inicial. La promesa está creada pero aún no se ha resuelto ni ha fallado.
+Fulfilled (resuelta): Indica que la promesa se ha completado con éxito.
+Rejected (rechazada): Ocurre cuando la promesa no se puede cumplir, produciendo un error.
 
-2 métodos en las promesas:
-- then(): se llama cuando la promesa se resuelve
-- catch(): se llama cuando la promesa no se resuelve
+¿Qué funciones de callback usan las promesas?
+Las promesas trabajan con dos tipos de funciones de callback:
+
+Resolve: Se ejecuta cuando la promesa se resuelve exitosamente.
+Reject: Se activa cuando la promesa no se puede completar, indicando un fallo.
+
+¿Cómo utilizar métodos como then y catch con promesas?
+Las promesas vienen con dos métodos esenciales para gestionar su resultado:
+
+Then: Se ejecuta cuando la promesa se resuelve. A través de este método, podemos llevar a cabo acciones basadas en el resultado exitoso de una promesa.
+Catch: Se utiliza para manejar errores. Si la promesa falla, el catch nos proporciona la información necesaria para entender por qué no se resolvió.
 */
 
 // Crear una nueva promesa
@@ -36,4 +41,28 @@ promise
   .catch((errorMessage) => {
     // Este bloque se ejecuta si la promesa se rechaza
     console.log(errorMessage); // Imprimir el mensaje de error
+  });
+
+// Crear otra promesa simulando una respuesta de API
+const myPromise = new Promise((resolve, reject) => {
+  let mockApiResponse = true; // Simulación de una respuesta de API
+
+  setTimeout(() => {
+    if (mockApiResponse) {
+      resolve('Operación completada con éxito'); // Llamar a resolve si la operación fue exitosa
+    } else {
+      reject('Error en la operación'); // Llamar a reject si la operación falló
+    }
+  }, 3000); // Esperar 3 segundos antes de resolver o rechazar la promesa
+});
+
+// Manejar la resolución de la promesa simulada
+myPromise
+  .then(result => {
+    // Este bloque se ejecuta si la promesa se resuelve
+    console.log(result); // Imprimir el mensaje de éxito
+  })
+  .catch(error => {
+    // Este bloque se ejecuta si la promesa se rechaza
+    console.error(error); // Imprimir el mensaje de error
   });
